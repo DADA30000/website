8b34e28d79c2c6477eba24ca9f832faa1815cab1
DADA30000 <vadimhack.ru@gmail.com>
2024-08-24 01:06:54 +0300

    lil update

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ -f ./check ]; then
+  git add . --all
+  git add .gitattributes
+  echo "Enter commit name (enter to default)"
+  read name
+  if [ -n "$name" ]; then
+    git commit -m "$name"
+    git push -u
+  else
+    git commit -m "lil update"
+    git push -u
+  fi
+else
+  echo "change your working directory to dotfiles already"
+fi
